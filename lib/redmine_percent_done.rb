7a42db18a22b60119f051a271c92077a9764f175
Brice VICO <brice.vico@smile.fr>
2017-04-03 16:50:08 +0200

    fix bug with parenthesis

--- lib/redmine_percent_done.rb
+++ lib/redmine_percent_done.rb
@@ -4,7 +4,7 @@ module RedminePercentDone
     ::RedminePercentDone::IssuePatch.apply
   end
 
-  def self.get_field_id(id)
+  def self.get_field_id
     unless (percent_done = Setting.plugin_redmine_percent_done["status-#field-to-link"]).blank?
       percent_done.to_i
     end
